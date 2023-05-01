f396054843dc423cf3664d8b6e46941158943efd
MonLes <73730090+Monles@users.noreply.github.com>
2023-05-01 07:37:43 +0000

    day 01

--- 02.py
+++ 02.py
@@ -2,4 +2,14 @@
 
 # 2) The results should be printed in a comma-separated sequence on a single line.
 
-# 3)Suppose the following input is supplied to the program: 8 Then, the output should be:40320
+# 3) Suppose the following input is supplied to the program: 8 Then, the output should be:40320
+
+def fac(x):
+  a = 1
+  for i in range(1, x+1):
+    a *= i
+    print(f"{i} : {a}")
+
+
+num = int(input("Type a num to compute the factorial: "))
+fac(num)
