d9cf473ffdb0b13d2692767a3f439532824b08b5
MonLes <73730090+Monles@users.noreply.github.com>
2023-05-01 07:44:32 +0000

    day 01

--- 02.py
+++ 02.py
@@ -4,11 +4,24 @@
 
 # 3) Suppose the following input is supplied to the program: 8 Then, the output should be:40320
 
+# 1) Write a program which can compute the factorial of a given numbers.
+
+# 2) The results should be printed in a comma-separated sequence on a single line.
+
+# 3) Suppose the following input is supplied to the program: 8 Then, the output should be:40320
+
+
 def fac(x):
   a = 1
-  for i in range(1, x+1):
-    a *= i
-    print(f"{i} : {a}")
+  if x <= 0:
+    print("1")
+  else:
+    try:
+      for i in range(1, x + 1):
+        a *= i
+        print(f"{i} : {a}")
+    except ValueError as err:
+      print(err)
 
 
 num = int(input("Type a num to compute the factorial: "))
