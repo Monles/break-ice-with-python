124ed7dbfcdf16c192ab44a57953abe16b4f0b84
MonLes <73730090+Monles@users.noreply.github.com>
2023-05-03 07:10:58 +0000

    day 03

---
+++ 07.py
@@ -0,0 +1,7 @@
+# Question 7
+# Question:
+# _Write a program which takes 2 digits, X,Y as input and generates a 2-dimensional array. The element value in the i-th row and j-th column of the array should be i _ j.*
+
+# Note: i=0,1.., X-1; j=0,1,¡­Y-1. Suppose the following inputs are given to the program: 3,5
+
+# Then, the output of the program should be:
---
+++ README.md
@@ -0,0 +1,5 @@
+Thanks for this awesomw project
+
+# Break The Ice With Python
+
+[Questions in Github](https://github.com/darkprinx/break-the-ice-with-python)
